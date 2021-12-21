5a3d9b8bf24da94cd5f2d4e508aa90af3e3f1c77
roxgib <33942237+roxgib@users.noreply.github.com>
2021-12-21 14:59:22 +1100

    initial implementation

--- __init__.py
+++ __init__.py
@@ -1,20 +1,44 @@
 from aqt import mw
-from aqt.utils import showInfo, qconnect
-from aqt.qt import qconnect
+from aqt.utils import qconnect, tooltip
+from aqt.qt import qconnect, QAction
+from aqt import gui_hooks
+
+from datetime import date
+import json
+
+ids_filename = '_ids.json'
 
 def buryUnburyNSFW() -> None:
-    if len(mw.col.find_cards("tag:buriedNSFW")):
-        # Unbury cards
-        ids = mw.col.find_cards("tag:buriedNSFW")
-        mw.col.unbury_cards(ids, manual=True)
-        mw.col.tags.bulk_remove(ids, 'buriedNSFW')
-        showInfo(f"Unburied {len(ids)} cards")
-    else:
+    try:
+        with open(ids_filename, 'r') as f:
+            ids = json.loads(f.read())
+    except:
+        ids = [0,'']
+
+    if ids[0] != str(date.today()) or ids[1] == '':
         # Bury cards
         ids = mw.col.find_cards("tag:nsfw is:due")
-        mw.col.tags.bulk_add(ids, 'buriedNSFW')
-        mw.col.bury_cards(ids, manual=True)
-        showInfo(f"Buried {len(ids)} cards")
+        mw.col.sched.bury_cards(ids)
+        tooltip(f"Buried {len(ids)} card{'' if {len(ids)} == 1 else 's'}.")
+        
+        with open(ids_filename, 'w') as f:
+            f.write(json.dumps([str(date.today()),list(ids)]))
+
+        if {len(ids)} != 0:
+            menuItem.setText("Unbury NSFW Cards")
+
+    else:
+        # Unbury cards
+        ids = ids[1]
+        mw.col.sched.unbury_cards(ids)
+        tooltip(f"Unburied {len(ids)} card{'' if {len(ids)} == 1 else 's'}.")
+        
+        with open(ids_filename, 'w') as f:
+            f.write(json.dumps([str(date.today()),'']))
+
+        menuItem.setText("Bury NSFW Cards")
+    
+    mw.reset()
 
 def filterNSFW() -> None:
     pass
@@ -22,6 +46,21 @@ def filterNSFW() -> None:
 def unfilterNSFW() -> None:
     pass
 
-button = QAction(f"{'B' if len(mw.col.find_cards("tag:buriedNSFW")) else 'Unb'}'ury NSFW cards", mw)
-qconnect(button.triggered, buryUnburyNSFW)
-mw.form.menuTools.addAction(button)
+def initialise() -> None:
+    try:
+        with open(ids_filename, 'r') as f:
+            ids = json.loads(f.read())
+    except:
+        ids = [0,'']
+
+    if ids[0] != str(date.today()) or ids[1] == '':
+        menuItem.setText("Bury NSFW Cards")
+    else:
+        menuItem.setText("Unbury NSFW Cards")
+
+    mw.reset()
+
+menuItem = QAction(f"Bury NSFW Cards", mw)
+qconnect(menuItem.triggered, buryUnburyNSFW)
+mw.form.menuTools.addAction(menuItem)
+gui_hooks.profile_did_open.append(initialise)
